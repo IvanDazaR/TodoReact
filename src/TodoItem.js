33a9e5eec314d9b0283f5d02e7b30528c1b0a26f
ivandaza <talivan0717@hotmail.com>
2022-10-17 16:51:27 -0500

    [ADD] todo list and some styles

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,11 +1,19 @@
 import React from "react";
+import './styles/TodoItem.scss';
 
 function TodoItem (props){
     return(
-        <li>
-            <span>C</span>
-            <p>{props.text}</p>
-            <span>X</span>
+        <li className="TodoItem">
+            <label>
+                <input type="checkbox" />
+                <span className="checkbox"/>
+            </label>
+            <p className={`TodoItem-p ${props.completed && 'TodoItem-p--complete'}`}>
+                {props.text}
+            </p>
+            <span className="Icon Icon-delete">
+                X
+            </span>
         </li>
     );
 }
