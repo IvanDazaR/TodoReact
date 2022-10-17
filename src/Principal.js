f07d3789f0e9e1ce316a2bb937ff693b25b80623
ivandaza <talivan0717@hotmail.com>
2022-10-17 14:55:58 -0500

    [ADD] book image

--- src/Principal.js
+++ src/Principal.js
@@ -2,14 +2,14 @@ import React from "react";
 import { CreateTodoButton } from "./CreateTodoButton";
 import { CreateTodo } from "./CreateTodo";
 import './styles/Principal.scss';
-
+import book from './images/book.svg';
 function Principal(){
     return(
         <div className="principal">
             <h1>Create a New Task</h1>
             <CreateTodo />
             <CreateTodoButton />
-            {/* <image src='./images/book.png'></image> */}
+            <img src={book} alt="Book-img"></img>
         </div>
     )
 }
