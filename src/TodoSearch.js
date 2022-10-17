40b1ca9bbe2ac20858d7c926dd45d6f3cd17209b
ivandaza <talivan0717@hotmail.com>
2022-10-17 18:18:39 -0500

    [ADD] events in the todoSearch

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -2,8 +2,15 @@ import React from "react";
 import './styles/TodoSearch.scss';
 
 function TodoSearch(){
+    const onSearchValueChange= (event)=>{
+        console.log(event.target.value);
+    }
     return(
-        <input className="TodoSearch" placeholder="Search your Task"/>
+        <input 
+            className="TodoSearch" 
+            placeholder="Search your Task"
+            onChange={onSearchValueChange}    
+        />
     );
 }
 export {TodoSearch};
