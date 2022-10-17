67034b914aa9c03befb3853aeceeed7985c70f25
ivandaza <talivan0717@hotmail.com>
2022-10-17 18:14:14 -0500

    [ADD] events for the check and delete items

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -2,16 +2,28 @@ import React from "react";
 import './styles/TodoItem.scss';
 
 function TodoItem (props){
+    const onComplete = () => {
+        alert(`${props.text} COMPLETED!`);
+    }
+    const onDelete = () => {
+        alert(`${props.text} DELETED!`);
+    }
     return(
         <li className="TodoItem">
             <label className="TodoItem-lable">
                 <input className="TodoItem-input" type="checkbox" />
-                <span className="checkbox"/>
+                <span 
+                    className="checkbox"
+                    onClick={onComplete}    
+                />
             </label>
             <p className={`TodoItem-p ${props.completed && 'TodoItem-p--complete'}`}>
                 {props.text}
             </p>
-            <span className="Icon Icon-delete">
+            <span 
+                className="Icon Icon-delete"
+                onClick={onDelete}
+            >
                 X
             </span>
         </li>
