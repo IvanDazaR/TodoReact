613a68fb2d81ecbeffe278980cf51aed89be64cc
ivandaza <talivan0717@hotmail.com>
2022-10-17 18:03:41 -0500

    [ADD] Event for the createTodobutton

--- src/CreateTodoButton.js
+++ src/CreateTodoButton.js
@@ -2,8 +2,16 @@ import React from "react";
 import './styles/CreateTodoButton.scss';
 
 function CreateTodoButton(){
+    const onClickButton = (msg)=>{
+        alert(msg)
+    }
     return(
-        <button className="CreateTodoButton">Create Task</button>
+        <button 
+            className="CreateTodoButton"
+            onClick={() =>onClickButton("Button Clicked!!")}
+        >
+            Create Task
+        </button>
     );
     
 }
