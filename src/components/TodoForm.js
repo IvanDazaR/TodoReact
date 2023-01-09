82cbaff17b7cf4883a2990e9925ffd20b9bcd19f
ivandaza <talivan0717@hotmail.com>
2023-01-09 15:29:30 -0500

    [FIX] delete height from the todoForm and add styles to the todo task

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -17,7 +17,7 @@ function TodoForm(props){
        }
     return(
         <div className="TodoForm">
-            <p>Do you want to delete "{props.textValue}" from your To Do List?</p>
+            <p>Do you want to delete "<span>{props.textValue}</span>" from your To Do List?</p>
             <div>
                 <button onClick={() => setOpenModal(false)} className="button cancel-button">Cancel</button>
                 <button onClick={onDelete} className="button accept-button">Accept</button>
